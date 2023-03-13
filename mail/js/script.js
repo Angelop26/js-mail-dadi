bc447bbf5c58f96f60a480d284a4683823087ee4
Angelop26 <angeloprospero26@gmail.com>
2023-03-13 13:02:14 +0100

    mail complete

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,16 @@
-alert('ciao')
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+const arrayEmail = ['caio@gmail.com', 'lolita@gmail.com', 'checcobomba@gmail.com']
+const userEmail = prompt('ciao utente, immetti qui la tua email')
+
+let result = 'la tua email non è registrata al sito'
+for (i = 0; i < arrayEmail.length; i++){
+    if(userEmail === arrayEmail[i]){
+        result = 'puoi accedere al sito'
+    }
+}
+console.log(arrayEmail);
+console.log(userEmail);
+console.log(result);
