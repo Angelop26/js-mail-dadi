8f0387c91f4e262a5c1b9cbd566c53da9c20236b
Angelop26 <angeloprospero26@gmail.com>
2023-03-13 13:25:43 +0100

    mail bonus

--- mail/js/script.js
+++ mail/js/script.js
@@ -2,15 +2,20 @@
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
 
-const arrayEmail = ['caio@gmail.com', 'lolita@gmail.com', 'checcobomba@gmail.com']
-const userEmail = prompt('ciao utente, immetti qui la tua email')
-
-let result = 'la tua email non è registrata al sito'
-for (i = 0; i < arrayEmail.length; i++){
-    if(userEmail === arrayEmail[i]){
-        result = 'puoi accedere al sito'
-    }
-}
+const arrayEmail = ['caio@gmail.com', 'lolita@gmail.com', 'checcobomba@gmail.com'] 
 console.log(arrayEmail);
-console.log(userEmail);
-console.log(result);
+
+const button = document.getElementById('button')
+button.addEventListener('click', function(){
+    
+    const userEmailInput = document.getElementById('usermail')
+    const userEmail = userEmailInput.value
+    let result = 'la tua email non è registrata al sito'
+    for (i = 0; i < arrayEmail.length; i++){
+        if(userEmail === arrayEmail[i]){
+            result = 'puoi accedere al sito'
+        }
+    }
+    console.log(userEmail);
+    console.log(result);
+})
